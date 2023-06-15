0c630bca662a1e70d6e6959592f91de0c5056d83
Travis Dawson <t.dawson@student.maastrichtuniversity.nl>
2023-06-15 16:15:46 +0200

    Pipelines data path updated

--- src/models/meta/pipelines.py
+++ src/models/meta/pipelines.py
@@ -34,7 +34,7 @@ import src.models.meta.silhouette_k_means as silhouette_k_means
 
 # Paths
 root_path = root_dir()  # Root path of the project
-data_path = '/data/processed/'  # Path to where data is stored
+data_path = '/data/obs_and_meta/processed/'  # Path to where data is stored
 save_path = '/models/meta/'  # '/notebooks/meta_modelling/model_comparison_cache/' to produce models for evaluation within the meta_data_model_comparison notebook
 
 # Boolean Flags
